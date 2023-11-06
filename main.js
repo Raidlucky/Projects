d328a5659469b81c5c00e1dc7709272da90f6ad8
Lucky <68414136+Raidlucky@users.noreply.github.com>
2023-11-06 05:21:02 -0700

    small animation
    idk if I'll keep the animation

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 function Open() {
     document.querySelector('.pop').style.display = "flex";
-    console.log('Open');
+    anime({
+        targets: '.pop',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 500
+    });
 }
 
 function Close() {
     document.querySelector('.pop').style.display = "none";
-    console.log('Close');
 }
+
+anime({
+    targets: '.main-contentText',
+    translateX: [-250, 0],
+    opacity: [0, 1],
+    easing: 'spring(1, 80, 10, 0)',
+    duration: 200
+})
