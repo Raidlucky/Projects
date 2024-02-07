ade0b71cf0dda7623ede5dec234fb52231680aa7
Lucky <68414136+Raidlucky@users.noreply.github.com>
2024-02-06 23:49:30 -0700

    Warning
    I'm not finishing the website for a bit, so this just lets me relax and focus on current projects

--- main.js
+++ main.js
@@ -19,3 +19,14 @@ anime({
     easing: 'spring(1, 80, 10, 0)',
     duration: 200
 })
+
+function dismissWarning() {
+    document.querySelector('.warning').style.display = "none";
+    anime({
+        targets: '.warning',
+        opacity: [1, 0],
+        easing: 'easeInOutQuad',
+        duration: 500
+    });
+    document.querySelector('nav').style.marginTop = "0";
+}
